2f545d7f2e53ab39fbf9cde9237095e7d534273e
Азамат <xismatullin2003@inbox.ru>
2023-03-13 10:21:25 +0300

    Добавил фукнцию mul, и создал файл strings с фукнцией up, импортировал в main.py модуль string.py фукнцию p

--- first/main.py
+++ first/main.py
@@ -1,3 +1,4 @@
+from strings import up
 def plus(a, b):
     return a + b
 
@@ -6,6 +7,10 @@ text = 'new text'
 def minus(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
 if __name__ == '__main__':
     print(plus(13, 53))
     print(minus(43, 16))
+    print(mul(5, 75))
+    print(up(text))
